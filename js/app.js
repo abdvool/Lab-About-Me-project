437ee658a4d5646db6ff7715e118b222bf87e59e
abdvool <abdvool@gmail.com>
2021-06-21 19:12:32 +0300

    new

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ if (username == 'YES' ) {
 }
 
 
-let age = prompt(' What is my Age now If I was Born in 1995  ').toUpperCase();
+let age = prompt(' What is my Age now If I was Born in 1995  ');
 switch (age) {
 case '26':
   console.log('correct');
@@ -48,7 +48,37 @@ while(password !== user){
 
 
 
+let gym = prompt('Can I lift up 200 KG?');
 
+switch (gym){
+
+case 'YES':
+  console.log('correct');
+  alert('thats correct ❚█══█❚ ');
+
+  break;
+
+default:
+
+  alert('I can left more than that ❚█══█❚ ? ');
+  console.log('wrong');
+}
+
+let languages = prompt('am I speak more than two languages ').toLowerCase();
+
+switch(languages){
+
+case('yes'):
+  alert('correct, I speak English,French,Arabic');
+  console.log('correct');
+  break;
+
+default:
+  alert('Wrong, I speak 3 languages' );
+  console.log('wrong');
+
+
+}
 
 
 
