983192581553a4245d392d7497ad4907116928a0
abdvool <abdvool@gmail.com>
2021-06-22 17:06:51 +0300

    12

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# Lab-About-Me-project
-
-
-![img]("../img/123.png")
+# Welcome
+------------
+My name is **Abdulrahman**, and I'm working on my website to show you some details about details and  advices you can benefit from them
--- js/app.js
+++ js/app.js
@@ -2,103 +2,149 @@
 
 
 
-let user = prompt('what\'s your name? ')
+// let user = prompt('what\'s your name? ')
 
-alert('welcome '+ user);
+// alert('welcome '+ user);
 
 
 
-let username = prompt('Is my name Abdulrahman?').toUpperCase();
+// let username = prompt('Is my name Abdulrahman?').toUpperCase();
 
-switch (username) {
+// switch (username) {
 
-  case 'YES':
-  case 'Y':
-  console.log('correct');
+//   case 'YES':
+//   case 'Y':
+//   console.log('correct');
  
-  alert('Correct');
+//   alert('Correct');
 
-  break;
+//   break;
 
-    case 'NO':
-    case 'N':
+//     case 'NO':
+//     case 'N':
 
-  alert('My name is Abdulrahman');
-  break;
+//   alert('My name is Abdulrahman');
+//   break;
 
-  default:
-  alert('you need to write yes or no ');
-  break;
-}
-
-
-let age = prompt(' What is my Age now If I was Born in 1995  ');
-switch (age) {
-case '26':
-  console.log('correct');
-  alert('correct');
-
-  break;
+//   default:
+//   alert('you need to write yes or no ');
+//   break;
+// }
 
 
-}
+// let age = prompt(' What is my Age now If I was Born in 1995  ');
+// switch (age) {
+// case '26':
+//   console.log('correct');
+//   alert('correct');
+
+//   break;
 
 
-
-
-let password = prompt('write your name here to continue ')
-
-while(password !== user){
-
-  password = prompt('you can\'t if you don\'t write it as before ' + user + ' ;)' );
-
-
-}
+// }
 
 
 
 
-let gym = prompt('Can I lift up 200 KG?').toUpperCase();
+// let password = prompt('write your name here to continue ')
 
-switch (gym){
+// while(password !== user){
 
-case 'YES':
-case 'Y':
-  console.log('correct');
-  alert('thats correct ❚█══█❚ ');
-
-  break;
-
-case 'NO':
-case 'N':
-
-  alert('I can left more than that ❚█══█❚ ? ');
-  console.log('wrong');
-  break;
+//   password = prompt('you can\'t if you don\'t write it as before ' + user + ' ;)' );
 
 
-}
+// }
 
-let languages = prompt('am I speak more than two languages ').toUpperCase();
 
-switch (languages){
 
-case('YES'):
-case('Y'):
-  alert('correct, I speak English,French,Arabic');
-  console.log('correct');
-  break;
 
-case ('NO'):
-case ('N'):
-  alert('Wrong, I speak 3 languages' );
-  console.log('wrong');
+// let gym = prompt('Can I lift up 200 KG?').toUpperCase();
+
+// switch (gym){
+
+// case 'YES':
+// case 'Y':
+//   console.log('correct');
+//   alert('thats correct ❚█══█❚ ');
+
+//   break;
+
+// case 'NO':
+// case 'N':
+
+//   alert('I can left more than that ❚█══█❚ ? ');
+//   console.log('wrong');
+//   break;
+
+
+// }
+
+// let languages = prompt('am I speak more than two languages ').toUpperCase();
+
+// switch (languages){
+
+// case('YES'):
+// case('Y'):
+//   alert('correct, I speak English,French,Arabic');
+//   console.log('correct');
+//   break;
+
+// case ('NO'):
+// case ('N'):
+//   alert('Wrong, I speak 3 languages' );
+//   console.log('wrong');
   
-  break;
+//   break;
 
+
+// }
+
+
+
+// alert('Thank your for your time '+ user);
+
+
+
+
+
+//  for (let i = 0; i < 4; i++) {
+//     let guess = prompt('guess the right number');
+//         if (guess <= 4) {
+//             alert('to low ')
+
+            
+//         }
+
+
+//       else if (guess == 5) {
+//           alert('correct answer')
+//           break;
+//         }
+// else if (guess > 5){
+//     alert('to high')
+
+// }  if (i == 3) {
+//     alert('your out of attempt your correct answer is 5 ' )
+    
+// }    }
+
+
+let richest = ['jeff', 'elon', 'bernard ',' bill ','mark'];
+
+  for (let i = 0; i < 6 ; i++) {
+
+    let richest = prompt('write  the richest guys in the world');
+
+    if( i == richest.length ){
+
+    alert('correct answer') 
+    
+} else if ( i != richest.length ){
+
+alert('wrong')
 
 }
 
 
 
-alert('Thank your for your time '+ user);
+}
