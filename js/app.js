18222c7ac6ab8be2649c8d87622b53bfe08c1b21
abdvool <abdvool@gmail.com>
2021-06-24 13:18:49 +0300

    edited

--- js/app.js
+++ js/app.js
@@ -1,214 +1,267 @@
-// 'use strict';
+'use strict';
 
 
 
-// let user = prompt('what\'s your name? ')
+let user = prompt('what\'s your name? ');
 
-// alert('welcome '+ user);
+alert('welcome '+ user);
 
-// let grade = 0;
+let grade = 0;
 
-// let username = prompt('Is my name ahmad?').toUpperCase();
+let username = prompt('Is my name ahmad?').toUpperCase();
 
-// function abd(){
-// switch (username) {
+function abd(){
+  switch (username) {
 
-// case 'YES':
-// case 'Y':
-//   console.log(false);
+  case 'YES':
+  case 'Y':
+    console.log(false);
 
-//   alert('wrong my name is Abdulrahman');
+    alert('wrong my name is Abdulrahman');
 
-//   break;
+    break;
 
-// case 'NO':
-// case 'N':
+  case 'NO':
+  case 'N':
 
-//   alert('correct');
-//   console.log(true);
+    alert('correct');
+    console.log(true);
 
-// grade++;
+    grade++;
 
-//   break;
+    break;
 
-// default:
-//   alert('you need to write yes or no ');
+  default:
+    alert('you need to write yes or no ');
 
-// }
-// }
-// abd();
+  }
+}
+abd();
 
 
-// let age = prompt(' What is my Age now If I was Born in 1995  ');
-// function abd1(){
-// switch (age) {
-// case '26':
-//   console.log(true);
-//   alert(true);
-//   grade++;
+let age = prompt(' if I born in 1995 is my age now 26?').toLowerCase();
 
-//   break;
+function abd1(){
+  switch (age) {
+  case 'yes':
+  case 'y':
+    console.log(true);
+    alert('correct');
+    grade++;
 
-// default:
-//   console.log(false);
-//   break;
+    break;
 
+  case'no':
+  case'n':
 
-// }
-// }
-// abd1();
+    console.log(false);
+    alert('wrong');
+    break;
 
+  default:
+    alert('you will lose points if you don\'t answer well');
+    break;
 
 
-// let password = prompt('write your name here to continue ')
-// function abd2(){
+  }
+}
+abd1();
 
-// while(password !== user){
 
-//   password = prompt('you can\'t if you don\'t write it as before ' + user + ' ;)' );
 
 
-// }
-// }
-// abd2();
+let password = prompt('write your name here to continue ');
+function abd2(){
 
+  while(password !== user){
 
+    password = prompt('you can\'t if you don\'t write it as before ' + user + ' ;)' );
 
-// let gym = prompt('Can I lift up 200 KG?').toUpperCase();
 
-// function abd3(){
+  }
+}
+abd2();
 
-// switch (gym){
+let live = prompt('Am I living in Amman?').toUpperCase();
 
-// case 'YES':
-// case 'Y':
-//   console.log(true);
-//   alert('thats correct ❚█══█❚ ');
-//   grade++;
+function abd7(){
 
-//   break;
+  switch (live) {
 
-// case 'NO':
-// case 'N':
+  case 'YES':
+  case 'Y':
+    alert('correct');
+    console.log('true');
+    break;
 
-//   alert('I can left more than that ❚█══█❚ ? ');
-//   console.log(false);
-//   break;
+  case 'NO':
+  case 'N':
+    alert('wrong');
+    console.log(false);
 
+    break;
 
-// }
-// }
-// abd3();
 
+  default:
+    alert('not answered in right way');
+    break;
 
 
-// let languages = prompt('am I speak more than two languages ').toUpperCase();
 
-// function abd4(){
-// switch (languages){
 
-// case('YES'):
-// case('Y'):
-//   alert('correct, I speak English,French,Arabic');
-//   console.log(true);
-//   grade++;
 
-//   break;
+  }
+}abd7();
 
-// case ('NO'):
-// case ('N'):
-//   alert('Wrong, I speak 3 languages' );
-//   console.log(false);
 
-//   break;
+let gym = prompt('Can I lift up 200 KG?').toUpperCase();
 
+function abd3(){
 
-// }
-// }
-// abd4();
+  switch (gym){
 
+  case 'YES':
+  case 'Y':
+    console.log(true);
+    alert('thats correct ❚█══█❚ ');
+    grade++;
 
+    break;
 
+  case 'NO':
+  case 'N':
 
+    alert('I can left more than that ❚█══█❚ ? ');
+    console.log(false);
+    break;
 
-// function abd5(){
-// for (let i = 0; i < 4; i++) {
-//   let guess = Number(prompt('guess the right number'));
-//   if (guess <= 4) {
-//     alert('to low ');
+  default:
+    alert('not answered in right way');
+    break;
 
 
-// }
 
+  }
+}
+abd3();
 
-//   else if (guess == 5) {
-//     alert('correct answer')
 
-//     console.log(true);
-//     grade++;
 
-//     break;
-//   }
+let languages = prompt('am I speak more than two languages ').toUpperCase();
 
-//   else if (guess > 5){
-//     alert('to high');
+function abd4(){
+  switch (languages){
 
-// } if (i == 3) {
-//     alert('your out of attempt your correct answer is 5 ' )
-//     console.log(false);
+  case('YES'):
+  case('Y'):
+    alert('correct, I speak English,French,Arabic');
+    console.log(true);
+    grade++;
 
-// }
+    break;
 
-// }
-// }
-// abd5();
+  case ('NO'):
+  case ('N'):
+    alert('Wrong, I speak 3 languages' );
+    console.log(false);
 
+    break;
 
+  default:
 
+    alert('please focus the answers is yes or no ');
+    break;
 
 
+  }
+}
+abd4();
 
 
 
 
 
-//   let rich =['elon', 'mark', 'jeff', 'bill'];
 
-//   let correct = false;
 
-//   function abd6(){
-//   for (let times = 1; times <= 6 ; times++) {
 
-//     let q = prompt('write 1 name of the 10 richest people').toLowerCase();
+function abd5(){
+  for (let i = 0; i < 4; i++) {
+    let guess = Number(prompt('guess the right number'));
+    if (guess <= 4) {
+      alert('to low ');
 
-//     for (let i = 0; i < rich.length; i++){
 
-//       if ( q == rich[i]) {
+    }
 
-//         alert('correct')
-//         times = 10;
-//         grade++;
 
-//         break;
+    else if (guess == 5) {
+      alert('correct answer');
 
-//       }
-//     }
-//     if (correct) {
-//       break;
-//     }
+      console.log(true);
+      grade++;
 
+      break;
+    }
 
+    else if (guess > 5){
+      alert('to high');
 
+    } if (i == 3) {
+      alert('your out of attempt your correct answer is 5 ' );
+      console.log(false);
 
-//   }
+    }
 
-// }
-// abd6();
+  }
+}
+abd5();
 
 
 
 
 
 
-// alert('your score is ' + grade)
-// alert('Thank your for your time '+ user);
+
+
+
+
+let rich =['elon', 'mark', 'jeff', 'bill'];
+
+let correct = false;
+
+function abd6(){
+  for (let times = 1; times <= 6 ; times++) {
+
+    let q = prompt('write 1 name of the 10 richest people').toLowerCase();
+
+    for (let i = 0; i < rich.length; i++){
+
+      if ( q === rich[i]) {
+
+        alert('correct');
+        times = 10;
+        grade++;
+
+        break;
+
+      }
+    }
+    if (correct) {
+      break;
+    }
+
+
+
+
+  }
+
+}
+abd6();
+
+
+
+
+
+
+alert('your score is ' + grade);
+alert('Thank your for your time '+ user);
