ac637b758e829cd03ab36da49ea28741634d7082
abdvool <abdvool@gmail.com>
2021-06-26 17:03:38 +0300

    ol

--- js/app.js
+++ js/app.js
@@ -1,225 +1,225 @@
-'use strict';
+// 'use strict';
 
 
 
-let user = prompt('what\'s your name? ');
+// let user = prompt('what\'s your name? ');
 
-alert('welcome ' + user);
+// alert('welcome ' + user);
 
-let grade = 0;
+// let grade = 0;
 
-let username = prompt('Is my name ahmad?').toUpperCase();
+// let username = prompt('Is my name ahmad?').toUpperCase();
 
-function abd() {
-  switch (username) {
+// function abd() {
+//   switch (username) {
 
-  case 'YES':
-  case 'Y':
-    console.log(false);
+//   case 'YES':
+//   case 'Y':
+//     console.log(false);
 
-    alert('wrong my name is Abdulrahman');
+//     alert('wrong my name is Abdulrahman');
 
-    break;
+//     break;
 
-  case 'NO':
-  case 'N':
+//   case 'NO':
+//   case 'N':
 
-    alert('correct');
-    console.log(true);
+//     alert('correct');
+//     console.log(true);
 
-    grade++;
+//     grade++;
 
-    break;
+//     break;
 
-  default:
-    alert('you need to write yes or no ');
-  }
-}
-abd();
+//   default:
+//     alert('you need to write yes or no ');
+//   }
+// }
+// abd();
 
 
-let age = prompt(' if I born in 1995 is my age now 26?').toLowerCase();
+// let age = prompt(' if I born in 1995 is my age now 26?').toLowerCase();
 
-function abd1() {
-  switch (age) {
-  case 'yes':
-  case 'y':
-    console.log(true);
-    alert('correct');
-    grade++;
+// function abd1() {
+//   switch (age) {
+//   case 'yes':
+//   case 'y':
+//     console.log(true);
+//     alert('correct');
+//     grade++;
 
-    break;
+//     break;
 
-  case 'no':
-  case 'n':
+//   case 'no':
+//   case 'n':
 
-    console.log(false);
-    alert('wrong');
-    break;
+//     console.log(false);
+//     alert('wrong');
+//     break;
 
-  default:
-    alert('you will lose points if you don\'t answer well');
-    break;
-  }
-}
-abd1();
+//   default:
+//     alert('you will lose points if you don\'t answer well');
+//     break;
+//   }
+// }
+// abd1();
 
 
-let putyourname = prompt('write your name here to continue ');
-function abd2() {
+// let putyourname = prompt('write your name here to continue ');
+// function abd2() {
 
-  while (putyourname !== user) {
+//   while (putyourname !== user) {
 
-    putyourname = prompt('you can\'t if you don\'t write it as before ' + user + ' ;)');
+//     putyourname = prompt('you can\'t if you don\'t write it as before ' + user + ' ;)');
 
-  }
-}
-abd2();
+//   }
+// }
+// abd2();
 
-let live = prompt('Am I living in Amman?').toUpperCase();
+// let live = prompt('Am I living in Amman?').toUpperCase();
 
-function abd7() {
+// function abd7() {
 
-  switch (live) {
+//   switch (live) {
 
-  case 'YES':
-  case 'Y':
-    alert('correct');
-    console.log('true');
-    break;
+//   case 'YES':
+//   case 'Y':
+//     alert('correct');
+//     console.log('true');
+//     break;
 
-  case 'NO':
-  case 'N':
-    alert('wrong');
-    console.log(false);
+//   case 'NO':
+//   case 'N':
+//     alert('wrong');
+//     console.log(false);
 
-    break;
+//     break;
 
-  default:
-    alert('not answered in right way');
-    break;
-  }
-} abd7();
+//   default:
+//     alert('not answered in right way');
+//     break;
+//   }
+// } abd7();
 
 
-let gym = prompt('Can I lift up 200 KG?').toUpperCase();
+// let gym = prompt('Can I lift up 200 KG?').toUpperCase();
 
-function abd3() {
+// function abd3() {
 
-  switch (gym) {
+//   switch (gym) {
 
-  case 'YES':
-  case 'Y':
-    console.log(true);
-    alert('thats correct ❚█══█❚ ');
-    grade++;
+//   case 'YES':
+//   case 'Y':
+//     console.log(true);
+//     alert('thats correct ❚█══█❚ ');
+//     grade++;
 
-    break;
+//     break;
 
-  case 'NO':
-  case 'N':
+//   case 'NO':
+//   case 'N':
 
-    alert('I can left more than that ❚█══█❚ ? ');
-    console.log(false);
-    break;
+//     alert('I can left more than that ❚█══█❚ ? ');
+//     console.log(false);
+//     break;
 
-  default:
-    alert('not answered in right way');
-    break;
-  }
-}
-abd3();
+//   default:
+//     alert('not answered in right way');
+//     break;
+//   }
+// }
+// abd3();
 
 
 
-let languages = prompt('am I speak more than two languages ').toUpperCase();
+// let languages = prompt('am I speak more than two languages ').toUpperCase();
 
-function abd4() {
-  switch (languages) {
+// function abd4() {
+//   switch (languages) {
 
-  case ('YES'):
-  case ('Y'):
-    alert('correct, I speak English,French,Arabic');
-    console.log(true);
-    grade++;
+//   case ('YES'):
+//   case ('Y'):
+//     alert('correct, I speak English,French,Arabic');
+//     console.log(true);
+//     grade++;
 
-    break;
+//     break;
 
-  case ('NO'):
-  case ('N'):
-    alert('Wrong, I speak 3 languages');
-    console.log(false);
+//   case ('NO'):
+//   case ('N'):
+//     alert('Wrong, I speak 3 languages');
+//     console.log(false);
 
-    break;
+//     break;
 
-  default:
+//   default:
 
-    alert('please focus the answers is yes or no ');
-    break;
-  }
-}
-abd4();
+//     alert('please focus the answers is yes or no ');
+//     break;
+//   }
+// }
+// abd4();
 
 
-function abd5() {
-  for (let i = 0; i < 4; i++) {
-    let guess = Number(prompt('guess the right number'));
-    if (guess <= 4) {
-      alert('to low ');
-    }
+// function abd5() {
+//   for (let i = 0; i < 4; i++) {
+//     let guess = Number(prompt('guess the right number'));
+//     if (guess <= 4) {
+//       alert('to low ');
+//     }
 
 
-    else if (guess === 5) {
-      alert('correct answer');
+//     else if (guess === 5) {
+//       alert('correct answer');
 
-      console.log(true);
-      grade++;
+//       console.log(true);
+//       grade++;
 
-      break;
-    }
+//       break;
+//     }
 
-    else if (guess > 5) {
-      alert('to high');
+//     else if (guess > 5) {
+//       alert('to high');
 
-    } if (i === 3) {
-      alert('your out of attempt your correct answer is 5 ');
-      console.log(false);
-    }
-  }
-}
-abd5();
+//     } if (i === 3) {
+//       alert('your out of attempt your correct answer is 5 ');
+//       console.log(false);
+//     }
+//   }
+// }
+// abd5();
 
 
 
-let rich = ['elon', 'mark', 'jeff', 'bill'];
+// let rich = ['elon', 'mark', 'jeff', 'bill'];
 
-let correct = false;
+// let correct = false;
 
-function abd6() {
-  for (let times = 1; times <= 6; times++) {
+// function abd6() {
+//   for (let times = 1; times <= 6; times++) {
 
-    let q = prompt('write 1 name of the 10 richest people').toLowerCase();
+//     let q = prompt('write 1 name of the 10 richest people').toLowerCase();
 
-    for (let i = 0; i < rich.length; i++) {
+//     for (let i = 0; i < rich.length; i++) {
 
-      if (q === rich[i]) {
+//       if (q === rich[i]) {
 
-        alert('correct');
-        times = 10;
-        grade++;
+//         alert('correct');
+//         times = 10;
+//         grade++;
 
-        break;
+//         break;
 
-      }
-    }
-    if (correct) {
-      break;
-    }
-  }
+//       }
+//     }
+//     if (correct) {
+//       break;
+//     }
+//   }
 
-}
-abd6();
+// }
+// abd6();
 
 
-alert('your score is ' + grade);
-alert('Thank your for your time ' + user);
+// alert('your score is ' + grade);
+// alert('Thank your for your time ' + user);
